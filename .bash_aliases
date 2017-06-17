615d793d22c97736c0e2f4ca6eb0838b21d1b432
Kathryn Lingel <kathryn@lingel.net>
2017-06-17 01:00:03 -0700

    Add setup.sh

--- .bash_aliases
+++ .bash_aliases
@@ -7,6 +7,10 @@ alias activate='. venv/bin/activate'
 
 alias rm='rm -I'
 
-export PATH=$PATH:$HOME/.local/bin:$HOME/installs
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH=$PATH:$HOME/.local/bin:$HOME/installs:$PYENV_ROOT/bin
 
 shrug(){ echo -n "¯\_(ツ)_/¯" | (xsel||pbcopy);echo "¯\_(ツ)_/¯ copied to your clipboard"; }
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Half script; half checklist of how to customize a new Ubuntu system with vim, tmux, and pyenv
+# Maybe someday it'll be all automatic!
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install ack-grep git python-pip tmux vim
+
+# Generate an SSH key and register it with github
+ssh-keygen # and press enter twice to generate key
+
+echo 'Enter github password:'
+curl -X POST -u katlings -d '{"title":"`echo $HOSTNAME`","key":"'"$(cat $HOME/.ssh/id_rsa.pub)"'"}' https://api.github.com/user/keys
+
+# dotfiles
+git clone git@github.com:katlings/dotfiles.git # and press enter to connect
+cp dotfiles/.* $HOME
+source $HOME/.bashrc
+
+# Vim plugins
+git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall # and quit when it finishes running
+
+# Pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+rebash
+pyenv install 2.7
+pyenv install 3.5.3
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+rebash
